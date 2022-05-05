425b170b3cfaffe8aee8bd161f3ea8ac73d0c3bd
kavurilg <98037503+kavurilg@users.noreply.github.com>
2022-05-05 23:05:38 +0530

    Add files via upload

---
+++ floop.py
@@ -0,0 +1,18 @@
+for n in range(5):
+    print(n)
+print('-'*50)
+for n in range(2,20,2):
+    print(n)
+print('-'*50)
+for n in range(5,10):
+    print(n)
+for n in range(5,10):
+    print("hello")
+print('-'*50)
+for a in range(1,11):     # 2 table
+    print(2*a)
+for a in range(1,11):
+   # print("2 * "+a+"= "+2*a)
+    print("2 *",a,"=",2*a)
+print('='*50)
+print("reverse logic of range() function...")
